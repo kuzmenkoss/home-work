19a8fa8dc83ba3451176d33fa63325b211d30b5e
Sergey Kuzmenko <kuzmenkoss@gmail.com>
2020-04-24 10:55:04 +0300

    24.04 pages 125-136

--- src/main/java/oreilly/battleSea/SimpleDotComTestDrive.java
+++ src/main/java/oreilly/battleSea/SimpleDotComTestDrive.java
@@ -3,10 +3,15 @@ package oreilly.battleSea;
 public class SimpleDotComTestDrive {
     public static void main(String[] args) {
         SimpleDotCom dot = new SimpleDotCom(); // создаем єкземпляр класса  SimpleDotCom
-        int[] area = {2, 3, 4}; // создаем массив для корабля с расположением на 2.3.4
-        dot.setLocationCells(area);
-        String userGuess = "2";
-        String result = dot.checkYourself(userGuess);
+        int[] area = {2, 3, 4}; // создаем массив для корабля с расположением на 2.3.4 (три последовательных числа из семи)
+        dot.setLocationCells(area); // вызываем сеттер корабля
+        String userGuess = "2"; // делаем ход от имени пользователя
+        String result = dot.checkYourself(userGuess); // вызываем метод checkYourself () объекта SimpleDotCom
+        String testResult = "НЕУДАЧА";
+        if (result.equals("ПОПАЛ")) {
+            testResult = "ПРОЙДЕН";
+        }
+        System.out.println(testResult);
 
     }
 }
